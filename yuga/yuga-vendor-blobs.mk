479fcb2415adfe0b6cec0ffd9bce1f0d466efc60
root <carlsberg07@hotmail.com>
2014-08-17 09:29:36 +0000

    yuga: adapted outgoing call fix
    Change-Id: I59f07db54a2b3693925dc431a0fd5dcd9bd0160c
    Signed-off-by: DHOMD <DHO@dho.im>

--- yuga/yuga-vendor-blobs.mk
+++ yuga/yuga-vendor-blobs.mk
@@ -38,8 +38,11 @@ PRODUCT_COPY_FILES += \
     vendor/sony/yuga/proprietary/etc/flashled_calc_parameters.cfg:system/etc/flashled_calc_parameters.cfg \
     vendor/sony/yuga/proprietary/lib/hw/nfc.msm8960.so:system/lib/hw/nfc.msm8960.so \
     vendor/sony/yuga/proprietary/lib/libaudcal.so:system/lib/libaudcal.so \
+    vendor/sony/yuga/proprietary/lib/libc.so:system/lib/libc.so \
     vendor/sony/yuga/proprietary/lib/libcald_server.so:system/lib/libcald_server.so \
     vendor/sony/yuga/proprietary/lib/liboemcamera.so:system/lib/liboemcamera.so \
+    vendor/sony/yuga/proprietary/lib/libril.so:system/lib/libril.so \
+    vendor/sony/yuga/proprietary/lib/libril-qc-qmi-1.so:system/lib/libril-qc-qmi-1.so \
     vendor/sony/yuga/proprietary/vendor/camera/flash.dat:system/vendor/camera/flash.dat \
     vendor/sony/yuga/proprietary/vendor/camera/LGI02BN0_IMX132.dat:system/vendor/camera/LGI02BN0_IMX132.dat \
     vendor/sony/yuga/proprietary/vendor/camera/product.dat:system/vendor/camera/product.dat \
